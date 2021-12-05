f6c447c7da910d02d91e2460f341c69ff901825c
Toni <toni.giorgino@gmail.com>
2021-12-05 12:08:41 +0100

    typo

--- ecl2/app.R
+++ ecl2/app.R
@@ -18,10 +18,7 @@ ui <- fluidPage(
     # Application title
     titlePanel("A Classification Model for Class A GPCR ECL2 domains"),
     h3("Abstract"),
-    p("The extracellular loop 2 (ECL2) is the longest and the most diverse loop among class A G protein-coupled receptors (GPCRs). We propose a 7-cluster classification scheme of currently resolved ECL2 domains based on volume overlaps and their intermolecular interactions with the other GPCR regions.
-
-
-."),
+    p("The extracellular loop 2 (ECL2) is the longest and the most diverse loop among class A G protein-coupled receptors (GPCRs). We propose a 7-cluster classification scheme of currently resolved ECL2 domains based on volume overlaps and their intermolecular interactions with the other GPCR regions."),
     h3("Reference"),
     # p("This interactive view accompanies the paper:"),
     p("See: A. Di Pizio et al.,",em("A Classification Model for the Second Extracellular Loop of Class A GPCRs, "),
